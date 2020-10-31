6c9ccbd3990b450d9a86fda366b7a148d2be4f9f
e <yucihent@sina.com>
2020-10-31 22:26:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -66,12 +66,7 @@ Vue.use(Badge)
 Vue.use(Divider)
 Vue.use(Empty)
 Vue.prototype.$message = message
-// import { message, Icon, BackTop, Skeleton, Tag } from 'ant-design-vue'
 
-// Vue.use(Icon)
-// Vue.use(BackTop)
-// Vue.use(Skeleton)
-// Vue.use(Tag)
 Vue.use(VueLazyload, {
   // error: 'dist/error.png',
   loading: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1599662145948&di=a34b11cc695afcff74b9c407b9cb1293&imgtype=0&src=http%3A%2F%2Fimg4.imgtn.bdimg.com%2Fit%2Fu%3D439157189%2C4163695168%26fm%3D214%26gp%3D0.jpg'
