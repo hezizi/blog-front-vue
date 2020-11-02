a5162c0c397c86317d05264a0c694eb9d1b72b57
yucihzz <yucihent@sina.com>
2020-11-02 19:15:24 +0800

    使用webpackhtmlplugin插件处理cdn

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,10 +1,26 @@
 /**
  * all used icons
  */
+
+// header导航部分icon
 export { default as HomeOutline } from '@ant-design/icons/lib/outline/HomeOutline'
 export { default as TagsOutline } from '@ant-design/icons/lib/outline/TagsOutline'
 export { default as BarsOutline } from '@ant-design/icons/lib/outline/BarsOutline'
 export { default as UserOutline } from '@ant-design/icons/lib/outline/UserOutline'
 export { default as GithubOutline } from '@ant-design/icons/lib/outline/GithubOutline'
+
+// 文章列表以及详情部分icon
 export { default as EyeOutline } from '@ant-design/icons/lib/outline/EyeOutline'
 export { default as ClockCircleOutline } from '@ant-design/icons/lib/outline/ClockCircleOutline'
+
+// message组件所需icon
+// success
+export { default as CheckCircleOutline } from '@ant-design/icons/lib/outline/CheckCircleOutline'
+// warning
+export { default as ExclamationCircleOutline } from '@ant-design/icons/lib/outline/ExclamationCircleOutline'
+// error
+export { default as CloseCircleOutline } from '@ant-design/icons/lib/outline/CloseCircleOutline'
+// info
+export { default as InfoCircleOutline } from '@ant-design/icons/lib/outline/InfoCircleOutline'
+// loading
+export { default as LoadingOutline } from '@ant-design/icons/lib/outline/LoadingOutline'
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
 const { join } = require('path')
 const resolve = dir => join(__dirname, dir)
 const CompressionPlugin = require("compression-webpack-plugin")
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+// cdn
+const cdn = {
+  css: ['https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.1/styles/atom-one-dark.min.css'],
+  js: [
+    'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.0.3/vue-router.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/vuex/3.0.1/vuex.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/marked/0.7.0/marked.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.2.0/highlight.min.js'
+  ]
+}
 
 module.exports = {
   productionSourceMap: false,
@@ -8,7 +24,12 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
+      /**
+       * Svg icons make bunlde size too large
+       * https://github.com/ant-design/ant-design/issues/12011
+       */
       .set('@ant-design/icons/lib/dist$', resolve('src/icons/index.js'))
+
     config.externals({
       vue: 'Vue',
       'vue-router': 'VueRouter',
@@ -17,23 +38,40 @@ module.exports = {
       highlight: 'hljs',
       marked: 'marked'
     })
+    config.plugin('html').tap(args => {
+      // html中添加cdn
+      args[0].cdn = cdn
+      return args
+    })
 
+    /**
+     * webpack loader that lets you use SVG files as Vue components
+     * https://www.npmjs.com/package/vue-svg-loader
+     */
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
     svgRule.use('vue-svg-loader').loader('vue-svg-loader')
 
-    // if (process.env.NODE_ENV === 'production') {
-    //   config
-    //   .plugin("compressionPlugin")
-    //   .use(CompressionPlugin)
-    //   .tap(() => [
-    //     {
-    //       test: /\.js$|\.html$|\.css/, //匹配文件名
-    //       threshold: 1024, //超过1k进行压缩
-    //       deleteOriginalAssets: false //是否删除源文件
-    //     }
-    //   ])
-    // }
+    if (isProd) {
+      /**
+       * Replace Moment.js with Day.js in Antd project
+       * https://www.npmjs.com/package/antd-dayjs-webpack-plugin
+       */
+      config
+        .plugin('antdDayjsWebpackPlugin')
+        .use(AntdDayjsWebpackPlugin)
+
+      // config
+      //   .plugin("compressionPlugin")
+      //   .use(CompressionPlugin)
+      //   .tap(() => [
+      //     {
+      //       test: /\.js$|\.html$|\.css/, //匹配文件名
+      //       threshold: 1024, //超过1k进行压缩
+      //       deleteOriginalAssets: false //是否删除源文件
+      //     }
+      //   ])
+    }
   },
   css: {
     loaderOptions: {
