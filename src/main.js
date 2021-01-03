f0cfd19a8b81f70a256dd4b8a33721bf315d9d5d
yucihzz <yucihent@sina.com>
2021-01-03 20:11:01 +0800

    feat: 首页文章列表懒加载图片改为本地图片

--- src/main.js
+++ src/main.js
@@ -81,9 +81,10 @@ message.config({
 })
 
 import VueLazyload from 'vue-lazyload'
+import loading from './assets/images/loading.gif'
 Vue.use(VueLazyload, {
   // error: 'dist/error.png',
-  loading: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1599662145948&di=a34b11cc695afcff74b9c407b9cb1293&imgtype=0&src=http%3A%2F%2Fimg4.imgtn.bdimg.com%2Fit%2Fu%3D439157189%2C4163695168%26fm%3D214%26gp%3D0.jpg'
+  loading
 })
 
 // 全局注册SvgIcon组件
@@ -94,7 +95,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  mounted () {
+  mounted() {
     store.commit('SET_TOKEN', getStorage('GITHUB_ACCESS_TOKEN'))
     store.commit('SET_USERINFO', getStorage('GITHUB_INFO'))
     store.dispatch('setAllTags')
